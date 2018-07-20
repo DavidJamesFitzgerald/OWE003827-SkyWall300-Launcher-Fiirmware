e93cec8f9445bc27ab6b803710b4a6fd3a2a880b
David Fitzgerald <david.fitzgerald@openworksengineering.com>
2018-07-20 10:26:21 +0100

    Created project. XTALs set.

---
+++ OWE003827-SkyWall300-Launcher-Firmware/OWE003827-SkyWall300-Launcher-Firmware/main.c
@@ -0,0 +1,32 @@
+/*    _____ __        _       __      _________ ____  ____     __                           __
+ *   / ___// /____  _| |     / /___ _/ / /__  // __ \/ __ \   / /   ____ ___  ______  _____/ /_  ___  _____
+ *   \__ \/ //_/ / / / | /| / / __ `/ / / /_ </ / / / / / /  / /   / __ `/ / / / __ \/ ___/ __ \/ _ \/ ___/
+ *  ___/ / ,< / /_/ /| |/ |/ / /_/ / / /___/ / /_/ / /_/ /  / /___/ /_/ / /_/ / / / / /__/ / / /  __/ /
+ * /____/_/|_|\__, / |__/|__/\__,_/_/_//____/\____/\____/  /_____/\__,_/\__,_/_/ /_/\___/_/ /_/\___/_/
+ *			/____/
+ */
+  
+/**
+ *	@file		main.c
+ *
+ *	@copyright	OpenWorks Engineering Ltd. 2018
+ *
+ *  @author		Created by David Fitzgerald (david.fitzgerald@openworksengineering.com)
+ *  @date		Last modified on 20 July 2018
+ *
+ *  @brief		Contains main state machine. This file is editable.
+ *
+ */
+  
+#include <atmel_start.h>
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+
+	/* Replace with your application code */
+	while(true)
+	{
+	}
+}
